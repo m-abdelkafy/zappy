0f7e0feccd892ced0fb5bcb85b55047637bee778
m.abdelkafy <m.abdelkafy@gmail.com>
2019-03-02 01:01:11 +0200

    adding config file and setting config to global variable

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const app = require('./app');
-const port = process.env.PORT || 3000;
+process.env.NODE_ENV = 'development';
+const config = require('./config');
+const port = global.gConfig.port || 3000;
 const server = app.listen(port, () => {
   console.log('Express server listening on port ' + port);
 });
